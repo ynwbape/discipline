2ec72519ebb066754d0de21b973f129e71fd2a99
Pierre NANGI <pierre.nangi@idmagna.fr>
2023-11-21 11:59:51 +0100

    background vectors almost finish

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+        backgroundColor: {
+            explosivegrey: '#C4C4C4',
+            gentianblue: '#211F97',
+            outoftheblue: '#13A3F4',
+            ultimatepink: '#FC57FF',
+            pinkdelight: '#FF88D7',
+            violetpink: '#FC62FF',
+            classicrose: '#FFCDE5',
+            candygrapefizz: '#6751ED',
+            
+        },
+        linearGradientColor: {
+            frozenlandscape: '#B4E8FF',
+            barbara: 'rgba(255, 24, 246, 0.48)',
+            neonblue: 'rgba(0, 224, 255, 0.00)',
+            purplehepatica: '#C4AFFF',
+        },
+        extend: {
+            colors: {
+                white: 'rgba(255, 255, 255, 0.00)'
+            }
+        } 
+    }
+}
